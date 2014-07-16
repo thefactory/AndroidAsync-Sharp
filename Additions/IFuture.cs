5605104ce5d15693f0fe5c142f8fe0fea6e584f5
Todd Berman <tberman@thefactory.com>
2014-07-16 13:59:21 -0700

    Since the Websocket construction calls return a Future, need this class to exist for it to be wrapped properly
    Test Plan: manual
    
    Reviewers: emuye, justin
    
    Reviewed By: justin
    
    Subscribers: justin, stephan, serdar
    
    Differential Revision: http://phabricator.thefactory.com/D2818

---
+++ Additions/IFuture.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Com.Koushikdutta.Async.Future {
+
+    public partial interface IFuture {
+        new bool IsCancelled { get; }
+        new bool IsDone { get; }
+    }
+}
+
